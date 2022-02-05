cd039016d0ea3c33210e9b3478f8e0689e326a87
Dobiarz <tomasz.tobiasz80@gmail.com>
2022-02-05 19:43:58 +0100

    clean code

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,19 +1,22 @@
 package tests;
 
+import com.utils.DriverFactory;
+import com.utils.DriverType;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public abstract class BaseTest {
 
     protected WebDriver driver;
 
-//    @BeforeMethod
-//    public void setup() {
-//        driver = DriverFactory.getDriver(DriverType.CHROME);
-//        driver.manage().window().maximize();
-//        driver.get("https://hello.friday.de/quote/selectPrecondition");
-//
-//    }
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.getDriver(DriverType.CHROME);
+        driver.manage().window().maximize();
+        driver.get("https://hello.friday.de/quote/selectPrecondition");
+
+    }
 
     @AfterMethod
     public void tearDown() {
