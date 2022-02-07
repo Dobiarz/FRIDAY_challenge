35707180050ae755f6ef49d691a127bcf1f16bc4
Dobiarz <tomasz.tobiasz80@gmail.com>
2022-02-07 12:54:29 +0100

    todo accept cookies

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -16,6 +16,21 @@ public abstract class BaseTest {
         driver.manage().window().maximize();
         driver.get("https://hello.friday.de/quote/selectPrecondition");
 
+        //TODO
+        // Accept Cookies
+        //        WebDriverWait wait = new WebDriverWait(driver, 5);
+        //        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(text(),'Alle akzeptieren')]")));
+
+        //        driver.switchTo().frame(0);
+        //        driver.findElement(By.xpath("//*[@id=\"uc-center-container\"]/div[2]/div/div/div/button[3]")).click();
+        //        driver.switchTo().defaultContent();
+
+        //        ChromeOptions options = new ChromeOptions();
+        //        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+
+        //        Actions actions = new Actions(driver);
+        //        WebElement body = driver.findElement(By.tagName("body"));
+        //        actions.moveToElement(body,327,408).click().build().perform();
     }
 
     @AfterTest
